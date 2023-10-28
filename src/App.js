8b2f907f4ba94637e4ae454f3644875d4f94cd32
unknown <drpheonixgaming2288@gmail.com>
2023-10-27 23:06:19 -0500

    Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-        // Can add more routes for more pages.
+          // Can add more routes for more pages.
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
