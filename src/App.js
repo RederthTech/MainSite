9c8de235f476991c49c3374241cbd20114ddebb4
unknown <drpheonixgaming2288@gmail.com>
2023-10-27 22:38:50 -0500

    Home added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App">Placeholder</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
