33c0e57d0e94aed6a07be02b91f1012549012b5d
unknown <drpheonixgaming2288@gmail.com>
2023-11-25 01:35:09 -0600

    Update citizenship

--- src/pages/citizenship/Citizenship.js
+++ src/pages/citizenship/Citizenship.js
@@ -1,41 +1,243 @@
-import React from
- 
-'react';
-import { Container, Row, Col } from
- 
-'react-bootstrap';
-import { Image } from
- 
-'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 
-export
- 
-default
- 
-function Citizenship() {
+export default function Citizenship() {
   return (
-    <Container style={{ backgroundColor: '#E0C9A6', fontFamily: 'Lobster' }}>
+    <Container style={{ backgroundColor: "#E0C9A6", fontFamily: "Lobster" }}>
       <Row>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdC8qK2s7RXuFmozwj3jgPYxbIIpEv8BvPd3cCNPq_Y8wkOFw/viewform?embedded=true" width="640" height="381" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-        <Col sm={12} md={6} className='mx-auto'>
+        <Col sm={12} md={6} className="mx-auto">
           <Image />
-          <div style={{ color: '#262626', fontFamily: 'Lobster' }}>
-            <h1 style={{ color: '#000' }}>Citizenship Laws</h1>
-            <p>Section 1A implies that a citizen should first fill out the citizenship application, then stay 1 month in anything related to Rederth online, or 3 months in anything related to Rederth in person before being called a citizen and gaining citizenship rights.</p>
-            <p>Section 2A implies that if a citizen were to leave the Grand Duchy of Rederth, online, for 24 hours or more, or if a citizen were to leave the Grand Duchy of Rederth, in person, for a year or longer, and return they would have to either wait another 3 months (if in person, otherwise 1 month), or fill out a legal document to show that you are still a citizen.</p>
-            <p>Section 3A implies a citizen cannot say extreme hate speech of any kind in the Rederth territory will be granted 1 warning before they will be removed of citizenship status.</p>
-            <p>Section 4A implies a citizen under the attack of excessive and severe hate speech the warning will not apply to the violator and will immediately get the removal of citizenship and wait 1 month before regaining it again.</p>
-            <p>Section 5A implies that a visitor of the nation wants to become a citizen they have the option to donate twice, the first donation skips five days off the 30-day timer and two donations will skip 10 days on the 30-day timer on becoming a citizen. The citizenship application is still required if you donate or not.</p>
-            <p>Section 6A implies that every citizen must pay monthly taxes, offsite citizens pay $5 US dollars while in person has to pay $15 US dollars</p>
-            <p>Section 7A implies that a citizen must complete 3 hours of labor for the government. offsite citizens get online labor, and in person citizens must do the following jobs given: digging, chopping, mining, etc.</p>
-            <p>Section 8A implies that a business can not be made private unless issued by the government.</p>
-            <h2 style={{ color: '#000' }}>Citizenship Rights</h2>
-            <p>Article I implies a citizen can use speech freely in Rederth territory, this does not include extreme hate speech.</p>
-            <p>Article II implies a citizen is granted full security within Rederth territory.</p>
-            <p>Article III implies a citizen in crisis can have a maximum of 5 local officers to safeguard you for 24-48 hours, the time chosen upon the request and severity of the situation.</p>
-            <p>Article IV implies that if a citizen is being attacked with excessive and severe hate speech it is an immediate, without warning, removal of citizenship to the attacker.</p>
-            <p>Article V implies that during times of an economic depression anyone can donate money to the government, citizens do not benefit from this except for escaping the depression. However, visitors who want to be a citizen can donate twice before they lose benefits.</p>
-            <p>Article VI implies </p>
+          <div style={{ color: "#262626", fontFamily: "sans-serif" }}>
+          <h6>"The Grand Duchy"/"Grand Duchy" hereby stand for The Grand Duchy of Rederth.</h6>
+            <h1 style={{ fontFamily: "lobster" }}>
+              <strong>Citizenship Requirements</strong>
+            </h1>
+            <p>
+              <strong style={{ fontFamily: "lobster" }}>Section 1A: </strong>All
+              prospective citizens of the Grand Duchy are{" "}
+              <strong>required</strong> to submit a lengthy citizenship
+              application. Subsequently, they <strong>must</strong> engage in{" "}
+              <strong>one month</strong> of <strong>online</strong> activity, or{" "}
+              <strong>three months</strong> of <strong>in-person</strong>{" "}
+              involvement in the Grand Duchy while their application is{" "}
+              <strong>extensively</strong> reviewed. Upon completion, the
+              applicant will be recognized as a citizen of the Grand Duchy, and
+              bestowed the <strong>rights of a citizen</strong>.
+            </p>
+            <p>
+              <strong style={{ fontFamily: "lobster" }}>Section 2A: </strong>In
+              the event that a citizen has departed from the Grand Duchy for{" "}
+              <strong>twenty-four hours or longer</strong>, if a{" "}
+              <strong>virtual citizen</strong>, or for{" "}
+              <strong>one-year or longer</strong>, if <strong>in-person</strong>
+              , their citizenship status will be <strong>revoked</strong>. Upon
+              return, they <strong>must</strong> perform the citizenship process
+              again.
+            </p>
+            <p>
+              <strong style={{ fontFamily: "lobster" }}>Section 3A: </strong>
+              Individuals are prohibited from violating any laws or regulations
+              within the Grand Duchy’s territory. If the individual is found to
+              have violated any laws or regulations, a strike will go towards
+              their application, and the proper legal process will be enacted.
+              Subsequent violations could result in the immediate denial of the
+              individual’s citizenship application as well as the proper legal
+              processes.
+            </p>
+            <p>
+              <strong style={{ fontFamily: "lobster" }}>Section 4A: </strong>In
+              severe and persistent violations of laws or regulations, the
+              warning provision will not apply and the offender will immediately
+              be denied citizenship and the proper legal processes will be
+              enacted. There is a mandatory <strong>six-month</strong> waiting
+              process before eligibility to reapply.
+            </p>
+            <p>
+              <strong style={{ fontFamily: "lobster" }}>Section 5A: </strong>
+              Prospective citizens have the opportunity to expedite the
+              citizenship review process through financial contribution. A
+              single donation reduces the <strong>30-day</strong> waiting
+              process by <strong>five days</strong>. A second donation reduces
+              the <strong>30-day</strong> waiting process by{" "}
+              <strong>ten days</strong>.{" "}
+              <strong>
+                NOTE: CITIZENSHIP APPLICATIONS REMAIN A PREREQUISITE REGARDLESS
+                OF DONATIONS MADE.
+              </strong>
+            </p>
+            <p>
+              <strong style={{ fontFamily: "lobster" }}>Section 6A: </strong>All
+              citizens are required to fulfill monthly tax obligations. Virtual
+              citizens are required to remit <strong>$5 U.S dollars</strong>,
+              while in-person citizens are required to remit{" "}
+              <strong>$15 U.S dollars</strong>. This tax may be substituted by
+              labor if financial contribution is not possible (More info in
+              Section 7A)
+            </p>
+            <p>
+              <strong style={{ fontFamily: "lobster" }}>Section 7A: </strong>In
+              the event that a citizen is unable to pay the monthly tax
+              obligation, they may substitute it with labor for the Grand Duchy.
+              Three hours minimum of labor each month financial tax is not
+              contributed. In-person citizens are able to fulfill this
+              obligation through physical activities, decided by the assignee
+              (Digging, chopping, mining, etc.), while virtual citizens may
+              complete online tasks decided by the assignee. (Writing documents,
+              answering questions, being an assistant, etc.)
+            </p>
+
+            <h1 style={{ fontFamily: "lobster" }}>
+              <strong>Citizen Rights</strong>
+            </h1>
+            <p style={{ fontStyle: "italic", fontFamily: "lobster" }}>
+              (First Ten)
+            </p>
+            <h5>
+              <strong style={{ fontFamily: "lobster" }}>
+                Article I: Freedom of Expression.
+              </strong>
+            </h5>
+            <p style={{ fontStyle: "italic" }}>
+              Expansion: Citizens of Rederth are assured their right to express
+              their opinions as long as their speech and actions remain within
+              legal and ethical boundaries. This emphasizes the importance of
+              self-expression.
+            </p>
+
+            <h5>
+              <strong style={{ fontFamily: "lobster" }}>
+                Article II: Security Assurance and Public Order.
+              </strong>
+            </h5>
+            <p style={{ fontStyle: "italic" }}>
+              Expansion: The Grand Duchy assures all citizens a secure living
+              environment. To maintain order and safety within public settings,
+              security and other precautions are implemented.
+            </p>
+
+            <h5>
+              <strong style={{ fontFamily: "lobster" }}>
+                Article III: Crisis Prevention Protocol.
+              </strong>
+            </h5>
+            <p style={{ fontStyle: "italic" }}>
+              Expansion: In instances of crisis, citizens have the right to
+              request temporary assistance from a limited number of officers.
+              This protocol ensures a prompt response without unneeded intrusion
+              during periods of normalcy.
+            </p>
+
+            <h5>
+              <strong style={{ fontFamily: "lobster" }}>
+                Article IV: Mitigation of Violence and Harmful Speech.
+              </strong>
+            </h5>
+            <p style={{ fontStyle: "italic" }}>
+              Expansion: The Grand Duchy takes a firm stance against any form of
+              severe harm or violence. This includes any speech or action that
+              directly incites or endorses harm. Prompt measures, within reason
+              and justice, are taken against individuals engaged in such harmful
+              discourse.
+            </p>
+
+            <h5>
+              <strong style={{ fontFamily: "lobster" }}>
+                Article V: Collective Support during Economic Challenges.
+              </strong>
+            </h5>
+            <p style={{ fontStyle: "italic" }}>
+              Expansion: In times of economic struggle, citizens are encouraged
+              to contribute financially. While direct personal benefits are not
+              attached, they signify a shared commitment to the support of the
+              Grand Duchy. Prospective citizens may reduce the time delay of
+              their integration through similar financial contributions.
+            </p>
+
+            <h5>
+              <strong style={{ fontFamily: "lobster" }}>
+                Article VI: Environmental Stewardship.
+              </strong>
+            </h5>
+            <p style={{ fontStyle: "italic" }}>
+              Expansion: Citizens are strongly encouraged to participate in the
+              preservation of the natural environment within the Grand Duchy’s
+              territory. The Grand Duchy promotes and implements sustainable
+              practices and may implement initiatives to ensure the longevity of
+              the natural world.
+            </p>
+
+            <h5>
+              <strong style={{ fontFamily: "lobster" }}>
+                Article VII: Educational Empowerment.
+              </strong>
+            </h5>
+            <p style={{ fontStyle: "italic" }}>
+              Expansion: The Grand Duchy is committed to fostering an educated,
+              knowledgeable, and informed citizenry. Educational opportunities
+              are widely and equally available, promoting access to educational
+              resources, and encouraging continual personal and professional
+              development.
+            </p>
+
+            <h5>
+              <strong style={{ fontFamily: "lobster" }}>
+                Article VIII: Cultural Diversity Recognition.
+              </strong>
+            </h5>
+            <p style={{ fontStyle: "italic" }}>
+              Expansion: The Grand Duchy values cultural diversity, recognizing
+              the unique culture and traditions of its citizens. The Grand Duchy
+              actively encourages the preservation of culture and tradition.
+            </p>
+
+            <h5>
+              <strong style={{ fontFamily: "lobster" }}>
+                Article IX: Equitable Healthcare Access.
+              </strong>
+            </h5>
+            <p style={{ fontStyle: "italic" }}>
+              Expansion: Every citizen is entitled to equal access to healthcare
+              services. The Grand Duchy endeavors to provide quality healthcare
+              and resources to ensure the physical and mental well-being of all
+              citizens.
+            </p>
+
+            <h5>
+              <strong style={{ fontFamily: "lobster" }}>
+                Article X: Technological Advancement and Innovation.
+              </strong>
+            </h5>
+            <p style={{ fontStyle: "italic" }}>
+              Expansion: The Grand Duchy strongly encourages technological
+              advancement. Citizens are encouraged to contribute and engage with
+              advancements in the technological field. This fosters an
+              environment that embraces innovation while ensuring ethical use.
+            </p>
+
+            <h5>
+              Click{" "}
+              <a
+                href={
+                  "https://docs.google.com/document/d/1WXpmyDwinJfjSUb6vBaLcOmYuxWYfiBIStFtJ3mIKAk/edit?usp=sharing"
+                }
+                target="_blank"
+              >
+                here
+              </a>{" "}
+              to read the rest of the <strong>Citizen Rights.</strong>
+            </h5>
+
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSdC8qK2s7RXuFmozwj3jgPYxbIIpEv8BvPd3cCNPq_Y8wkOFw/viewform?embedded=true"
+              width="100%"
+              height="900px"
+              frameborder="0"
+              marginheight="0"
+              marginwidth="0"
+            >
+              Loading…
+            </iframe>
           </div>
         </Col>
       </Row>
